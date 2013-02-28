2c60ed77f82e502969a23020d40d161e153c817b
Alan Harris <aharris@taximagic.com>
2013-02-27 21:07:33 -0500

    Adding carousel (with placekitten images)
    - probably going to want those images to live in a model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
+  before_filter :carousel_images, :only => :index
+
   def index
     @title = "Beth's Site"
+  end
+
+  protected
+
+  def carousel_images
+    @images = [
+      { :index => 1, :img => 'http://placekitten.com/1500/500' },
+      { :index => 2, :img => 'http://placekitten.com/1501/500' },
+      { :index => 3, :img => 'http://placekitten.com/1502/500' }
+    ]
   end
 end
