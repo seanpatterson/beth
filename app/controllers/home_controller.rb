cf48fa6925dfa08d5cea4345f59bdc9fca700417
psuparrot <seanpatterson@psualum.com>
2013-06-01 19:42:08 -0400

    Changed some text.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,9 +9,9 @@ class HomeController < ApplicationController
 
   def carousel_images
     @images = [
-      { :index => 1, :img => 'http://placekitten.com/1500/500' },
-      { :index => 2, :img => 'http://placekitten.com/1501/500' },
-      { :index => 3, :img => 'http://placekitten.com/1502/500' }
+      { :index => 1, :img => 'https://s3.amazonaws.com/bethpattersonart/jewelry/IMG_4630.JPG'  },
+      { :index => 2, :img => 'https://s3.amazonaws.com/bethpattersonart/painting/3+Generations%2C+28%22x24%22%2C+Oil+Paint+on+Linen%2C+2008.JPG' },
+      { :index => 3, :img => 'https://s3.amazonaws.com/bethpattersonart/Printmaking/IMG_5583.JPG' }
     ]
   end
 end
