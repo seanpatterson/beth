d844fb5f56f31b895ba71c07d6e209d7e998ef43
psuparrot <seanpatterson@psualum.com>
2013-06-02 13:45:52 -0400

    Fixed carousel image size.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,9 +9,9 @@ class HomeController < ApplicationController
 
   def carousel_images
     @images = [
-      { :index => 1, :img => 'https://s3.amazonaws.com/bethpattersonart/jewelry/IMG_4630.JPG'  },
-      { :index => 2, :img => 'https://s3.amazonaws.com/bethpattersonart/painting/3+Generations%2C+28%22x24%22%2C+Oil+Paint+on+Linen%2C+2008.JPG' },
-      { :index => 3, :img => 'https://s3.amazonaws.com/bethpattersonart/Printmaking/IMG_5583.JPG' }
+      { :index => 1, :img => 'https://s3.amazonaws.com/bethpattersonart/misc/Carousel+-IMG_5583.JPG'  },
+      { :index => 2, :img => 'https://s3.amazonaws.com/bethpattersonart/misc/Carousel-IMG_4630.JPG' },
+      { :index => 3, :img => 'https://s3.amazonaws.com/bethpattersonart/misc/Carousel-PearlsAndSuckers.jpg' }
     ]
   end
 end
